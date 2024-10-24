930d3b8983ca3b4ffa1387038063367953ce82ba
Minnnning <6alswjd6@gmail.com>
2024-10-24 22:40:49 +0900

    build: 프로젝트 초기 설정

---
+++ SpaceON/SpaceONApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceONApp.swift
+//  SpaceON
+//
+//  Created by 김민정 on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SpaceONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
